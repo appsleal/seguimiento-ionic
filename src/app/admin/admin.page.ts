4fb2258d3874ea0bdaee113220d2aa5d2bdf2e1f
hermes portatil <appsleal@gmail.com>
2022-06-07 21:42:53 -0500

    admin base

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -6,41 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-  navigate: any;
 
   constructor() {
-    this.sideMenu();
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  sideMenu() {
-    this.navigate = [
-      {
-        title: 'App',
-        url: '/apps',
-        icon: 'apps',
-      },
-      {
-        title: 'Book',
-        url: '/book',
-        icon: 'book',
-      },
-      {
-        title: 'Paint',
-        url: '/paint',
-        icon: 'brush',
-      },
-      {
-        title: 'Contacts',
-        url: '/contacts',
-        icon: 'contacts',
-      },
-      {
-        title: 'Facebook',
-        url: '/facebook.com',
-        icon: 'logo-facebook',
-      },
-    ];
-  }
+  navigate = [
+    {
+      title: 'Municipios',
+      url: 'municipios',
+    }
+  ];
 }

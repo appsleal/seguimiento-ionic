e1a5b2e62eb45152938131d1a8c41960e349ecf5
Jdawd <JuanDawdB@gmail.com>
2022-06-16 17:04:35 -0500

    feat: :children_crossing: Now the user gets a tiny warning about the uniqueness of both variables

--- src/app/creator/components/creeate-form/creeate-form.component.ts
+++ src/app/creator/components/creeate-form/creeate-form.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastController } from '@ionic/angular';
@@ -15,17 +17,29 @@ import { DocumentosService } from 'src/app/services/documentos.service';
 })
 export class CreeateFormComponent implements OnInit {
   formGroup: FormGroup;
-  public labelAttribute:string;
-  public objects:any[];
-  public options:AutoCompleteOptions;
-  public tipo_cedula:AutoCompleteOptions;
-  public telefono:AutoCompleteOptions;
-  public ciudad:AutoCompleteOptions;
-  pattern = "[A-Z ]";
+  public labelAttribute: string;
+  public objects: any[];
+  public options: AutoCompleteOptions;
+  public tipo_cedula: AutoCompleteOptions;
+  public telefono: AutoCompleteOptions;
+  public ciudad: AutoCompleteOptions;
+  pattern = '[A-Z ]';
 
- 
+  numberErrors = {
+    MSISDN_LINEA: 'El numero de telefono de la sim ya esta tomado',
+    NUMERO_DE_DOCUMENTO: 'El numero de documento ya existe',
+    variable: '',
+  };
 
-  constructor(private formBuilder: FormBuilder, public service: DataserviceService, public documento:DocumentosService, public departamento:DepartamentoService, private creatorservice: CreatorService, public numeroschips:ChipsService,public toastController: ToastController) { 
+  constructor(
+    private formBuilder: FormBuilder,
+    public service: DataserviceService,
+    public documento: DocumentosService,
+    public departamento: DepartamentoService,
+    private creatorservice: CreatorService,
+    public numeroschips: ChipsService,
+    public toastController: ToastController
+  ) {
     this.options = new AutoCompleteOptions();
     this.options.autocomplete = 'on';
     this.options.type = 'search';
@@ -44,89 +58,118 @@ export class CreeateFormComponent implements OnInit {
     this.telefono.placeholder = 'Ingresar el numero de telefono de la chip';
   }
 
-
   ngOnInit() {
     this.formGroup = this.formBuilder.group({
-      MSISDN_LINEA: ['', [Validators.required,Validators.pattern('[0-9 ]*')]],
-      TIPO_BENEFICIARIO:['', [Validators.required]],
-      NOMBRE_COMPLETO: ['', [Validators.required,Validators.pattern('[A-Z ]*')],],
+      MSISDN_LINEA: ['', [Validators.required, Validators.pattern('[0-9 ]*')]],
+      TIPO_BENEFICIARIO: ['', [Validators.required]],
+      NOMBRE_COMPLETO: [
+        '',
+        [Validators.required, Validators.pattern('[A-Z ]*')],
+      ],
       TIPO_DE_DOCUMENTO: ['', [Validators.required]],
-      NUMERO_DE_DOCUMENTO: ['', [Validators.required,Validators.pattern('[0-9 ]*')]],
+      NUMERO_DE_DOCUMENTO: [
+        '',
+        [Validators.required, Validators.pattern('[0-9 ]*')],
+      ],
       NUMERO_DE_TELEFONO: ['', [Validators.required]],
       MUNICIPIO_BENEFICIARIO: ['', [Validators.required]],
-      DIRECCION_BARRIO_BENEFICIARIO: ['', [Validators.required,Validators.pattern('[A-Z0-9 ]*')]],
-      NOMBRE_COMPLETO_TUTOR: ['',[,Validators.pattern('[A-Z ]*')]],
-      TELEFONO_CONTACTO_TUTOR: ['',[Validators.pattern('[0-9 ]*')]],
-      NUMERO_DOCUMENTO_TUTOR: ['',[Validators.pattern('[0-9 ]*')]],
-      NOMBRE_INSTITUCION_EDUCATIVA: ['', [Validators.required,,Validators.pattern('[A-Z ]*')]],
-
+      DIRECCION_BARRIO_BENEFICIARIO: [
+        '',
+        [Validators.required, Validators.pattern('[A-Z0-9 ]*')],
+      ],
+      NOMBRE_COMPLETO_TUTOR: ['', [, Validators.pattern('[A-Z ]*')]],
+      TELEFONO_CONTACTO_TUTOR: ['', [Validators.pattern('[0-9 ]*')]],
+      NUMERO_DOCUMENTO_TUTOR: ['', [Validators.pattern('[0-9 ]*')]],
+      NOMBRE_INSTITUCION_EDUCATIVA: [
+        '',
+        [Validators.required, , Validators.pattern('[A-Z ]*')],
+      ],
     });
 
-    this.formGroup.controls.NOMBRE_COMPLETO.valueChanges.subscribe((value: string) => {
-      this.formGroup.controls.NOMBRE_COMPLETO.setValue(value.toUpperCase(), 
-      {emitEvent: false});
-    })
+    this.formGroup.controls.NOMBRE_COMPLETO.valueChanges.subscribe(
+      (value: string) => {
+        this.formGroup.controls.NOMBRE_COMPLETO.setValue(value.toUpperCase(), {
+          emitEvent: false,
+        });
+      }
+    );
 
-    this.formGroup.controls.DIRECCION_BARRIO_BENEFICIARIO.valueChanges.subscribe((value: string) => {
-      this.formGroup.controls.DIRECCION_BARRIO_BENEFICIARIO.setValue(value.toUpperCase(), 
-      {emitEvent: false});
-    })
+    this.formGroup.controls.DIRECCION_BARRIO_BENEFICIARIO.valueChanges.subscribe(
+      (value: string) => {
+        this.formGroup.controls.DIRECCION_BARRIO_BENEFICIARIO.setValue(
+          value.toUpperCase(),
+          { emitEvent: false }
+        );
+      }
+    );
 
-    this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.valueChanges.subscribe((value: string) => {
-      this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.setValue(value.toUpperCase(), 
-      {emitEvent: false});
-    })
+    this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.valueChanges.subscribe(
+      (value: string) => {
+        this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.setValue(
+          value.toUpperCase(),
+          { emitEvent: false }
+        );
+      }
+    );
 
-    this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.valueChanges.subscribe((value: string) => {
-      this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.setValue(value.toUpperCase(), 
-      {emitEvent: false});
-    })
+    this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.valueChanges.subscribe(
+      (value: string) => {
+        this.formGroup.controls.NOMBRE_COMPLETO_TUTOR.setValue(
+          value.toUpperCase(),
+          { emitEvent: false }
+        );
+      }
+    );
   }
 
-  onKey(event){
-    var pattern = "[A-Za-z ]";
-    if(!event.key.match(pattern)){
-      var data:string = this.formGroup.get("NOMBRE_COMPLETO").value;
-      
+  onKey(event) {
+    const pattern = '[A-Za-z ]';
+    if (!event.key.match(pattern)) {
+      const data: string = this.formGroup.get('NOMBRE_COMPLETO').value;
+
       this.formGroup.patchValue({
-        "NOMBRE_COMPLETO":data.slice(0,data.length-1)
-      })
-      
+        NOMBRE_COMPLETO: data.slice(0, data.length - 1),
+      });
     }
   }
 
-  nombreTutor(event){
-    var pattern = "[A-Za-z ]";
-    if(!event.key.match(pattern)){
-      var data:string = this.formGroup.get("NOMBRE_COMPLETO_TUTOR").value;
-      
+  nombreTutor(event) {
+    const pattern = '[A-Za-z ]';
+    if (!event.key.match(pattern)) {
+      const data: string = this.formGroup.get('NOMBRE_COMPLETO_TUTOR').value;
+
       this.formGroup.patchValue({
-        "NOMBRE_COMPLETO_TUTOR":data.slice(0,data.length-1)
-      })
-      
+        NOMBRE_COMPLETO_TUTOR: data.slice(0, data.length - 1),
+      });
     }
   }
 
   async presentToast(text) {
     const toast = await this.toastController.create({
       message: text,
-      duration: 10000
+      duration: 10000,
     });
     toast.present();
   }
 
-  create(){
-    let data = this.formGroup.value;
-    
-    data['DEPARTAMENTO_BENEFICIARIO']=this.departamento.getDepartamento(this.formGroup.get("MUNICIPIO_BENEFICIARIO").value)
-    data['SERIAL_SIM']=this.numeroschips.getSerial(this.formGroup.get("MSISDN_LINEA").value)
-    this.creatorservice.createfile(data).subscribe(res=>{
-      if(res["success"]==true){
-        this.presentToast("Creado exitosamte");
-      }else{
-        this.presentToast(res["message"])
+  create() {
+    const data = this.formGroup.value;
+
+    data.DEPARTAMENTO_BENEFICIARIO = this.departamento.getDepartamento(
+      this.formGroup.get('MUNICIPIO_BENEFICIARIO').value
+    );
+    data.SERIAL_SIM = this.numeroschips.getSerial(
+      this.formGroup.get('MSISDN_LINEA').value
+    );
+    this.creatorservice.createfile(data).subscribe((res) => {
+      if (res['success'] === true) {
+        this.presentToast('Creado exitosamte');
+        // setTimeout(() => {
+        //   window.location.reload();
+        // }, 800);
+      } else {
+        this.numberErrors.variable = res['message'];
       }
-      
-    }) 
+    });
   }
 }
